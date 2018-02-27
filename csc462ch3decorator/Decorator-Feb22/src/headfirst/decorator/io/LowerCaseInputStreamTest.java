f484bb0bc598afda28d7314bb5c3059147535077
caseodilla <caseodilla@gmail.com>
2018-02-27 00:36:45 -0500

    finished testing LowerCaseInputStream

--- csc462ch3decorator/Decorator-Feb22/src/headfirst/decorator/io/LowerCaseInputStreamTest.java
+++ csc462ch3decorator/Decorator-Feb22/src/headfirst/decorator/io/LowerCaseInputStreamTest.java
@@ -7,36 +7,32 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class LowerCaseInputStreamTest {
-
-  @BeforeAll
-  static void setUpBeforeClass() throws Exception {
-  }
-
-  @AfterAll
-  static void tearDownAfterClass() throws Exception {
-  }
+  InputStream in;
+  ByteArrayOutputStream out;
+  String testString;
 
   @BeforeEach
   void setUp() throws Exception {
+    testString = "This is a TEST";
+    in = new LowerCaseInputStream(new ByteArrayInputStream(testString.getBytes()));
+    out = new ByteArrayOutputStream();
   }
 
   @AfterEach
   void tearDown() throws Exception {
+    in.close();
+    out.close();
   }
 
   @Test
   void testRead1() {
     int c;
-    String testString = "This is a TEST";
-    InputStream in = new LowerCaseInputStream(new ByteArrayInputStream(testString.getBytes()));
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    
     try {
       while((c = in.read()) > -1) {
         out.write(c);
@@ -55,10 +51,8 @@ class LowerCaseInputStreamTest {
   
   @Test
   void testRead2() {
-    String testString = "This is a TEST";
     byte[] buffer = new byte[testString.length()];
-    InputStream in = new LowerCaseInputStream(new ByteArrayInputStream(testString.getBytes()));
-    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    
     try {
       in.read(buffer, 0, testString.length());
       out.write(buffer);
