2725e2a6f4749cc216957cbdab15f30d5d926265
Casey Hayes <caseodilla@gmail.com>
2018-03-13 19:55:11 -0400

    added CalculatorApplet

---
+++ CalculatorApplet/src/edu/elon/applet/CalculatorApplet.java
@@ -0,0 +1,14 @@
+package edu.elon.applet;
+
+import javax.swing.JApplet;
+
+@SuppressWarnings("deprecation")
+public class CalculatorApplet extends JApplet {
+
+  private static final long serialVersionUID = 1L;
+
+  public void init() {
+    
+  }
+  
+}
