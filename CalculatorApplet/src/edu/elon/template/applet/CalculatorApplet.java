05e23724023c7e15a1aea1cd8311d8fb686e8652
caseodilla <caseodilla@gmail.com>
2018-03-13 23:42:32 -0400

    most of calculator is working

---
+++ CalculatorApplet/src/edu/elon/template/applet/CalculatorApplet.java
@@ -0,0 +1,97 @@
+package edu.elon.template.applet;
+
+import java.awt.BorderLayout;
+import java.awt.Container;
+import java.awt.GridLayout;
+
+import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+@SuppressWarnings("deprecation")
+public class CalculatorApplet extends JApplet {
+
+  private static final long serialVersionUID = 1L;
+  
+  private Container container;
+  private JTextField result;
+  private JPanel panel;
+  private JButton button;
+  
+  private Double firstNum;
+  private String op;
+  private boolean newNumber = true;
+  private boolean hasDecimal = false;
+
+  public void init() {
+    container = getContentPane();
+    
+    drawTextField();
+    drawButtons();
+  }
+  
+  private void drawTextField() {
+   result = new JTextField("0");
+   container.add(result, BorderLayout.NORTH);
+  }
+  
+  private void drawButtons() {
+    panel = new JPanel();
+    panel.setLayout(new GridLayout(4,4));
+    
+    String[] buttons = {"7","8","9","/","4","5","6","*","1","2","3","-","0",".","=","+"};
+    for (String b : buttons) {
+      button = new JButton(b);
+      button.addActionListener(e -> doSomething(b));
+      panel.add(button);
+    }
+    container.add(panel, BorderLayout.CENTER);
+  }
+  
+  public void doSomething(String s) {
+    try {
+      Integer.parseInt(s);
+      if (newNumber) {
+        result.setText(s);
+        newNumber = false;
+      } else {
+        result.setText(result.getText() + s);
+      }
+    } catch (NumberFormatException | NullPointerException e) {
+      String lastChar = result.getText().substring(result.getText().length()-1);
+      if (s.equals(".") && !lastChar.equals(".") && !hasDecimal) {
+        result.setText(result.getText() + ".");
+        hasDecimal = true;
+      } else {
+        if (firstNum == null) {
+          firstNum = Double.parseDouble(result.getText());
+        }
+        if (op != null) {
+          result.setText(operate(Double.parseDouble(result.getText())).toString());
+          op = null;
+          firstNum = null;
+        } else {
+          op = s;
+        }
+        hasDecimal = false;
+        newNumber = true;
+      }
+    }
+  }
+  
+  public Double operate(Double secondNum) {
+    switch (op) {
+    case "+":
+      return firstNum + secondNum;
+    case "-":
+      return firstNum - secondNum;
+    case "*":
+      return firstNum * secondNum;
+    case "/":
+      return firstNum / secondNum;
+    }
+    return secondNum;
+  }
+  
+}
